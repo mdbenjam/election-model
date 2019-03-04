137a5f5e0e939af33e0c359f18c3133511e281a7
Mark Benjamin <mdbenjam@gmail.com>
2019-03-04 15:06:59 +0000

    Adding docker file and basic source file

---
+++ image/entrypoint.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mdbenjam/election-model.git
+
+Rscript election-model/src/main.r
+upload_results.sh
---
+++ src/main.r
@@ -0,0 +1,3 @@
+print("Hello world")
+
+write([1, 2, 3, 4], file = "output/data", sep = " ")
