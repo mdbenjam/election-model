974131ed9bce0591b469cbfddb986d06484a0008
Mark Benjamin <mdbenjam@gmail.com>
2019-03-04 15:36:31 +0000

    Updating upload_results script

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/mdbenjam/election-model.git
 
 Rscript election-model/src/main.r
-upload_results.sh
+./upload_results.sh
--- image/upload_results.sh
+++ image/upload_results.sh
@@ -1,4 +1,7 @@
+cd election-model
 git_hash=$(git rev-parse HEAD)
+cd ..
+
 timestamp=$(date +'%m-%d-%Y-%R')
 
 echo "Uploading results to: " s3://election-model-output/$git_hash/$timestamp
